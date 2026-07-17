c31d4fcc46b2070f8e6f27362aa2163690e47f66
homemaro <mrejrfam.2@gmail.com>
2026-07-17 10:45:38 +0900

    add:prizesのjs・css(modal部分)作成

--- js/script.js
+++ js/script.js
@@ -35,3 +35,19 @@ const aboutSlider = new Swiper(".about__slider", {
 setTimeout(() => {
   aboutSlider.params.speed = 4000;
 }, 2000 * 2);
+
+document.querySelectorAll(".js-modal-open").forEach((btn) => {
+  btn.addEventListener("click",() => {
+    const id = btn.dataset.modal;
+    document.querySelector(`#modal-${id}`).classList.add("is-open");
+  });
+});
+
+document.querySelectorAll(".prize-modal").forEach((modal) => {
+  modal.addEventListener("click", (e) =>{
+    if(e.target.closest(".js-modal-close") || e.target === modal){
+      modal.classList.remove("is-open");
+    }
+  });
+});
+
